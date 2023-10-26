3119ef2c1ebc144035b2ecdf44d60ab6c590a350
Bram Groothedde <BramGroothedde@gmail.com>
2023-10-26 14:20:46 +0200

    feat: relocate bubble

--- scripts/content.js
+++ scripts/content.js
@@ -9,7 +9,7 @@ function renderCreateCommunityNoteButton() {
   // create a circle element that is always positioned on the right bottom of the screen
   const bubble = document.createElement('button');
   bubble.style.position = 'fixed';
-  bubble.style.top = '20px';
+  bubble.style.bottom = '20px';
   bubble.style.right = '20px';
   bubble.style.width = '35px';
   bubble.style.height = '35px';
@@ -19,6 +19,7 @@ function renderCreateCommunityNoteButton() {
   bubble.style.cursor = 'pointer';
   bubble.style.border = 'none';
   bubble.style.zIndex = '9999';
+  bubble.style.boxShadow = '0 0 5px rgba(0, 0, 0, 0.3)';
 
   bubble.textContent = 'C';
 
