77923c63b00fb1d55c2c8287fcf5a788161e7466
Bram Groothedde <BramGroothedde@gmail.com>
2023-10-26 14:09:20 +0200

    feat: add bubble

--- scripts/content.js
+++ scripts/content.js
@@ -1,46 +1,11 @@
 console.log('content script loaded');
 
-function createButton() {
-  const button = document.createElement('button');
-  button.style.backgroundColor = 'red';
-  button.style.color = 'white';
-  button.textContent = 'x';
-  button.style.borderRadius = '50%';
-  button.style.border = 'none';
-  button.style.width = '20px';
-  button.style.height = '20px';
-  button.style.position = 'absolute';
-  button.style.top = '0';
-  button.style.right = '0';
-  button.style.zIndex = '9999';
-  button.style.cursor = 'pointer';
-  button.addEventListener('click', () => {});
-
-  return button;
-}
-
+let selectedText = '';
 function handleSelection() {
-  const isButtonAdded = false;
   document.addEventListener('selectionchange', () => {
-    if (isButtonAdded) return;
-
-    const selection = document.getSelection();
-    if (!selection) return;
-    const range = selection.getRangeAt(0);
-    const button = createButton();
-
-    // append button to the body
-    document.body.appendChild(button);
-
-    // get boundingbox of the parentElement
-    const boundingBox = range.getBoundingClientRect();
-    console.log('>>> boundingBox', boundingBox);
-
-    // change the position of the button to the boundingBox
-    button.style.top = `${boundingBox.top}px`;
-    button.style.right = `${boundingBox.right}px`;
-
-    isButtonAdded = true;
+    const range = document.getSelection();
+    if (!range) return;
+    selectedText = range.toString();
   });
 }
 
@@ -48,9 +13,29 @@ function highlightRange(range) {
   console.log('>>> highlighting text', range.toString());
 }
 
-function renderBubble() {
-  console.log('>>> rendering bubble');
+function renderCreateCommunityNoteButton() {
+  // create a circle element that is always positioned on the right bottom of the screen
+  const bubble = document.createElement('button');
+  bubble.style.position = 'fixed';
+  bubble.style.top = '20px';
+  bubble.style.right = '20px';
+  bubble.style.width = '35px';
+  bubble.style.height = '35px';
+  bubble.style.borderRadius = '50%';
+  bubble.style.backgroundColor = 'grey';
+  bubble.style.color = 'white';
+  bubble.style.cursor = 'pointer';
+  bubble.style.border = 'none';
+  bubble.style.zIndex = '9999';
+
+  bubble.textContent = 'C';
+
+  document.body.appendChild(bubble);
+
+  bubble.addEventListener('click', () => {
+    console.log('selected text:', selectedText);
+  });
 }
 
-renderBubble();
+renderCreateCommunityNoteButton();
 handleSelection();
