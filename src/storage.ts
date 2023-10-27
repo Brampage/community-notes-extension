b17d8f66e088bc2575fb575931fd22df57c12402
Bram Groothedde <BramGroothedde@gmail.com>
2023-10-27 13:40:41 +0200

    store note

--- src/storage.ts
+++ src/storage.ts
@@ -9,9 +9,9 @@ export interface Note {
 export async function storeNote(key: string, note: Note) {
   const notes = await getNotes(key);
 
-  notes[key].push(note);
+  notes.push(note);
 
-  chrome.storage.local.set(notes);
+  chrome.storage.local.set({[key]: notes});
 }
 
 export async function getNotes(key: string) {
